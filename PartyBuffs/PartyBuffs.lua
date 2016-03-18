08ebeba313988521415f566f62f56f259d8fdda1
KenshiDRK <jaciarriaga@ono.com>
2016-03-18 17:57:00 +0100

    Update PartyBuffs.lua

--- PartyBuffs/PartyBuffs.lua
+++ PartyBuffs/PartyBuffs.lua
@@ -167,9 +167,9 @@ windower.register_event('prerender', function()
                     elseif member.zone == zone then
                         if buff[member_id[k]][i] ~= 255 and buff[member_id[k]][i] ~= 0 then
                             -- Adjust position for party member count
-                            if i >= 17 then
+                            if i ~= nil and i >= 17 then
                                 image:pos_y(party_buffs_y_pos[party_info.party1_count] + (((k-1)*20)+10))
-                            elseif i <= 16 then
+                            elseif i ~= nil and i <= 16 then
                                 image:pos_y(party_buffs_y_pos[party_info.party1_count] + ((k-1)*20))
                             end
                             image:path(windower.windower_path .. 'addons/PartyBuffs/icons/' .. buff[member_id[k]][i] .. '.png')
