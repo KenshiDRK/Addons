1cae8bb44dfe9d023fa035d9d3744fc7f3c07d28
KenshiDRK <jaciarriaga@ono.com>
2017-07-09 02:06:31 +0200

    Refactoring
    refactored to avoid the use of prerender event, it now only updates
    buffs with every pt buffs packet

--- PartyBuffs/PartyBuffs.lua
+++ PartyBuffs/PartyBuffs.lua
@@ -1,4 +1,4 @@
---[[Copyright © 2016, Kenshi
+--[[Copyright © 2017, Kenshi
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -28,17 +28,12 @@ _addon.name = 'PartyBuffs'
 _addon.author = 'Kenshi'
 _addon.version = '1.0'
 
-config = require('config')
 images = require('images')
 packets = require('packets')
 require('pack')
 require('tables')
 
-defaults = {}
-
-settings = config.load(defaults)
-
-party_buffs = {'p1', 'p2', 'p3', 'p4', 'p5'}
+local party_buffs = {'p1', 'p2', 'p3', 'p4', 'p5'}
 
 do
     local x_pos = windower.get_windower_settings().ui_x_res - 190
@@ -66,9 +61,9 @@ do
   
 end
 
-member_table = T{}
+local member_table = S{nil, nil, nil, nil, nil}
 
-buffs = {}
+local buffs = T{}
 
 windower.register_event('incoming chunk', function(id, data)
         
@@ -79,6 +74,7 @@ windower.register_event('incoming chunk', function(id, data)
             member_table:append(packet['Name'])
             member_table[packet['Name']] = packet['ID']
         end
+        coroutine.schedule(Update, 1)
         
     end
     
@@ -90,17 +86,23 @@ windower.register_event('incoming chunk', function(id, data)
             
             if id ~= 0 then
                 for i = 1, 32 do
-                    buffs[id][i] = data:byte(k*48+5+16+i-1) + 256*( math.floor( data:byte(k*48+5+8+ math.floor((i-1)/4)) / 4^((i-1)%4) )%4) -- Credit: Byrth, GearSwap
+                    local buff = data:byte(k*48+5+16+i-1) + 256*( math.floor( data:byte(k*48+5+8+ math.floor((i-1)/4)) / 4^((i-1)%4) )%4) -- Credit: Byrth, GearSwap
+                    if buffs[id][i] ~= buff then
+                        buffs[id][i] = buff
+                    end
                 end
             end
         end
+        Update()
         
     end
     
     if id == 0xB then
         zoning_bool = true
+        Update()
     elseif id == 0xA and zoning_bool then
         zoning_bool = false
+        coroutine.schedule(Update, 10)
     end
     
 end)
@@ -111,36 +113,30 @@ for i = 2, 6 do
     party_buffs_y_pos[i] = y_pos - 20 * i
 end
 
-windower.register_event('prerender', function()
+function Update()
     local party_info = windower.ffxi.get_party_info()
     local zone = windower.ffxi.get_info().zone
     local party = windower.ffxi.get_party()
-    local key_indices = {'p1', 'p2', 'p3', 'p4', 'p5',}
+    local key_indices = {'p1', 'p2', 'p3', 'p4', 'p5'}
    
     for k = 1, 5 do
         local member = party[key_indices[k]]
-        
-        if member and member.mob then
-            if not member.mob.is_npc and not member_table:contains(member.name) then
-                member_table:append(member.name)
-                member_table[member.name] = member.mob.id
-            end
-        end
         
         for image, i in party_buffs[k]:it() do
             if member then
                 if buffs[member_table[member.name]] and buffs[member_table[member.name]][i] then
                     if zoning_bool then
-                        if buffs[member_table[member.name]][i] ~= 0 then
-                            buffs[member_table[member.name]][i] = 0
-                        end
+                        buffs[member_table[member.name]][i] = 0
+                        image:clear()
                         image:hide()
                     elseif member.zone ~= zone then
-                        if buffs[member_table[member.name]][i] ~= 0 then
-                            buffs[member_table[member.name]][i] = 0
-                        end
+                        buffs[member_table[member.name]][i] = 0
+                        image:clear()
                         image:hide()
-                    elseif buffs[member_table[member.name]][i] ~= 255 and buffs[member_table[member.name]][i] ~= 0 then
+                    elseif buffs[member_table[member.name]][i] == 255 or buffs[member_table[member.name]][i] == 0 then
+                        image:clear()
+                        image:hide()
+                    else
                         image:path(windower.windower_path .. 'addons/PartyBuffs/icons/' .. buffs[member_table[member.name]][i] .. '.png')
                         -- Adjust position for party member count
                         if party_info.party1_count > 1 then
@@ -151,18 +147,33 @@ windower.register_event('prerender', function()
                             end
                         end
                         image:show()
-                    else
-                        image:hide()
                     end
-                else
-                    image:hide()
                 end
             else
+                image:clear()
                 image:hide()
             end
             image:update()
         end
-    
+        
     end
     
+end
+
+windower.register_event('load', function() --Create member table if addon is loaded while already in pt
+    if not windower.ffxi.get_info().logged_in then return end
+    
+    local party = windower.ffxi.get_party()
+    local key_indices = {'p1', 'p2', 'p3', 'p4', 'p5'}
+    
+    for k = 1, 5 do
+        local member = party[key_indices[k]]
+        
+        if member and member.mob then
+            if not member.mob.is_npc and not member_table:contains(member.name) then
+                member_table[k] = member.name
+                member_table[member.name] = member.mob.id
+            end
+        end
+    end
 end)
